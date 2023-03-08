e97916a61c3f89eda51ba8d9a323202cf9ebbe99
Enzo Viry <enzo.viry@gmail.com>
2023-03-08 17:27:17 +0100

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,9 +109,9 @@ module.exports = {
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
 				// The property ID; the tracking code won't be generated without it
-				trackingId: "UA-158680515-1",
+				trackingId: "G-9GPE2HB9JD",
 				// Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: false,
+				head: true,
 				// Setting this parameter is optional
 				anonymize: true,
 				// Setting this parameter is also optional
